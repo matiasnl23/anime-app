7dbb8f4d1e62fb9e59f8f7ad595f781d7f0abebf
Matías Lobo <matiasnl.1993@gmail.com>
2021-03-13 01:34:23 -0300

    Creación del header

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, NavbarComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
